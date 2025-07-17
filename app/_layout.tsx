550823affb35c4d8d5d75f2d554879e1dd716bb6
Tom <hello@tsaunders.dev>
2025-07-17 21:57:17 +0100

    Add Marker.io feedback tracking script to app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,26 @@
 // app/_layout.tsx
 import { Slot } from 'expo-router';
+import Head from 'expo-router/head';
 import '../global.css'; // this path is correct because global.css is one level above
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.markerConfig = {
+                project: '687963305d957cb25651d0ca', 
+                source: 'snippet'
+              };
+
+              !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","clearReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
+            `,
+          }}
+        />
+      </Head>
+      <Slot />
+    </>
+  );
 }
